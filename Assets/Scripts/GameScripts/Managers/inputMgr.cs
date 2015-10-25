eac380769955d5c8f5972b63ec0cf95ba5bb125b
Riasse <riasse13@yahoo.es>
2015-10-26 00:09:48 +0100

    Menu

--- Assets/Scripts/GameScripts/Managers/inputMgr.cs
+++ Assets/Scripts/GameScripts/Managers/inputMgr.cs
@@ -7,23 +7,32 @@ using System.Collections;
 /// También por ejemplo, cuando pulsemos la tecla de pause, para que se cargue la nueva escena encima de la del juego y se pare todo. Es decir,
 /// eventos de entrada, bien sobre elementos UI, bien con atajos de teclado para retroceder sobre menús o hacer pause en el juego. 
 /// </summary>
+///
 public class inputMgr : MonoBehaviour {
 
-    bool paused;
+    public bool paused;
 
 	// Use this for initialization
 	void Start () 
     {
         paused = false;
+
+        
 	}
 	 
 	// Update is called once per frame
 	void Update () 
     {
         //Si estamos en la escena de juego y pulsamos la tecla de pause
+        
         if (Application.loadedLevelName == "game" && Input.GetButtonDown("Pause"))
+         {
+             pauseGame();           
+         }
+
+        if (paused == false)
         {
-            pauseGame();           
+            resumeGame();
         }
 	}
 
@@ -34,6 +43,18 @@ public class inputMgr : MonoBehaviour {
 
         //Que aparezca el menú de pausa.
 
+        GameObject menuPausa = Instantiate(Resources.Load("Menu Pausa", typeof(GameObject))) as GameObject;
+
 
     }
+
+    public void resumeGame()
+    {
+        Time.timeScale = 1.0F;
+
+        //Destuimos el menú
+
+        Destroy(GameObject.Find("Menu Pausa(Clone)"));
+    }
+
 }
